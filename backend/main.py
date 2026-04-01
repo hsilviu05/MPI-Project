1717d563d889ee5c32fad57d1f5cd38840ed3927
IftimeRazvan <razvaniftime24@gmail.com>
2026-04-01 19:30:25 +0300

    feat: add initial database setup and configuration management

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,22 @@
 from fastapi import FastAPI
 from .core.config import settings
+from .db import init_db, close_db
+
 
 app = FastAPI(
     title=settings.app_name,
     version="1.0.0"
 )
+
+
+@app.on_event("startup")
+def on_startup():
+    init_db()
+
+
+@app.on_event("shutdown")
+def on_shutdown():
+    close_db()
 
 
 @app.get("/health")
